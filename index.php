e4dbfb216a73db24b18e693de9ac27c760f05186
Kathleen Mattos <kathleenmmattos@gmail.com>
2019-01-14 11:56:25 -0700

    created project for demo

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html Lang="en">
+	<head>
+		<meta charset="utf-8"/>
+		<title>Basic Demo of a project Creation</title>
+	</head>
+	<body>
+		<h1>Deep Dive Fullstack Cohort</h1>
+	</body>
+</html>
